8ed6f53813f6bad53c3e234e20626bd17853cf3c
Scott Brown <sbrown9797@gmail.com>
2025-01-10 13:21:12 -0800

    Remove unused code from main

--- c/src/main.cc
+++ c/src/main.cc
@@ -1,29 +1,13 @@
-#include <algorithm>
-#include <cstdint>
-#include <iterator>
-#include <ostream>
-#include <ppl.hh>
-#include <stdio.h>
 #include <time.h>
-#include <type_traits>
-#include <vector>
-#include <boost/numeric/interval.hpp>
+#include <stdio.h>
 
 #include "invariant.hh"
 
 void tests(void);
 
-using namespace Parma_Polyhedra_Library;
 using namespace std;
-using namespace boost::numeric;
-using namespace interval_lib;
-
-
-static Variable x(0);
-static Variable y(1);
 
 uint8_t stop = 0;
-
 
 int main() {
     //tests();
@@ -31,25 +15,17 @@ int main() {
     IntervalData Omega({interval_t(-6, 6), interval_t(-6, 6)});
 
     auto start = clock();
-    vector<IntervalData> res;
-    uint64_t ii = 0;
+    vector<IntervalData> res = {Omega};
+    uint64_t i = 0;
     while (!stop) {
-        if (ii == 0) {
-            res = I_approx({Omega});
-        } else {
-            res = I_approx(res);
-        }
-        ii++;
+        res = I_approx(res);
+        i++;
     }
     auto end = clock();
-    printf("Computed %ld iters I_approx %.10f seconds\n\n", ii, (double)(end - start)/CLOCKS_PER_SEC);
+    printf("Computed %ld iters I_approx %.10f seconds\n\n", i,
+           (double)(end - start)/CLOCKS_PER_SEC);
 
     /*
-    print_points(res);
-
-    cout << res.size() << endl;
-
-
     cout << "inputs:\n";
     auto U = U_approx(res);
     for (auto i = U.begin(); i!=U.end(); ++i) {
@@ -57,12 +33,6 @@ int main() {
         print_points(*i);
         cout << "}\n";
     }
-    start = clock();
-    auto res2 = I_accel({Omega});
-    end = clock();
-    printf("Computed I_accel %.10f seconds\n\n", (double)(end - start)/CLOCKS_PER_SEC);
-
-    cout << res.size() << endl;
     */
 
     return 0;
