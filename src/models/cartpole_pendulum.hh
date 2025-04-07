554f4e14d217d7cfdcee056f94581c81134aa7e1
Scott Brown <sbrown9797@gmail.com>
2025-04-07 15:44:18 -0700

    Fix cartpole model

--- src/models/cartpole_pendulum.hh
+++ src/models/cartpole_pendulum.hh
@@ -1,4 +1,11 @@
-//TODO finish model
+#include <cstdint>
+static const string DATA_DIR = "data_cartpole_pendulum/";
+static const nvec_t epsilon = {1e-1, 1e-1};
+
+static interval_t U(-10, 10);
+static ninterval_t Omega_0 = {interval_t(-3, 3),
+                              interval_t(-10, 10)};
+
 
 inline C_Polyhedron A(nvec_t x_m, C_Polyhedron P) {
     const int64_t A[NDIM][NDIM] = {{10, 1}, {0, 10}};
@@ -12,16 +19,19 @@ inline C_Polyhedron A(nvec_t x_m, C_Polyhedron P) {
 }
 
 inline C_Polyhedron B(nvec_t x_m, interval_t U) {
-    const int64_t B[NDIM] = {5, -20};
-    const int64_t B_den = 100;
+    const int64_t B_den = INT16_MAX*10;
+    const int64_t B[NDIM] = {
+        0,
+        rat_approx(cos(x[0]) / (0.12 * pow(cos(x[0]), 2) - 0.96), INT16_MAX)
+    };
 
     const int64_t U_den = INT16_MAX;
     static int64_t Ul = rat_approx(U.lower(), U_den);
     static int64_t Uh = rat_approx(U.upper(), U_den);
 
     C_Polyhedron res(2, EMPTY);
-    res.add_generator(point(B[0]*Ul*Variable(0) + B[1]*Ul*Variable(1), B_den*U_den));
-    res.add_generator(point(B[0]*Uh*Variable(0) + B[1]*Uh*Variable(1), B_den*U_den));
+    res.add_generator(B[1]*Ul*Variable(1), B_den*U_den));
+    res.add_generator(B[1]*Uh*Variable(1), B_den*U_den));
 
     return res;
 }
