b3c84a7768c50538e76440d58418eff306e5fc09
Scott Brown <sbrown9797@gmail.com>
2025-04-10 11:25:25 -0700

    Print function now prints interval instead of polytope

--- src/print.cc
+++ src/print.cc
@@ -21,40 +21,10 @@ void print_points(const C_Polyhedron& P, ostream& f) {
     f << "]\n";
 }
 
-void fprint_points(const C_Polyhedron& P, string fname, bool append) {
-    ofstream f;
-    if (append) {
-        f.open(fname, ios_base::app);
-    } else {
-        f.open(fname);
+void print_points(const ninterval_t& x, ostream& f) {
+    f << "[";
+    for (int i = 0; i < NDIM; i++) {
+        f << "[" << x[i].lower() << "," << x[i].upper() << "];";
     }
-
-    print_points(P, f);
-}
-
-void print_points(const vector<IntervalData>& P) {
-    for (const IntervalData& p : P) {
-        print_points(p.poly);
-    }
-}
-
-void fprint_points(const vector<IntervalData>& P, string fname) {
-    bool append = false;
-    for (const IntervalData& p : P) {
-        fprint_points(p.poly, fname, append);
-        append = true;
-    }
-}
-
-void print_points(const vector<C_Polyhedron>& P) {
-    for (const C_Polyhedron& p : P) {
-        print_points(p);
-    }
-}
-void fprint_points(const vector<C_Polyhedron>& P, string fname) {
-    bool append = false;
-    for (const C_Polyhedron& p : P) {
-        fprint_points(p, fname, append);
-        append = true;
-    }
+    f << "]\n";
 }
--- src/print.hh
+++ src/print.hh
@@ -1,8 +1,39 @@
+#ifndef PRINT_HH
+#define PRINT_HH
+
+#include <fstream>
 #include "set_types.hh"
 
-void print_points(const vector<IntervalData>& P);
-void fprint_points(const vector<IntervalData>& P, string fname);
-void print_points(const vector<C_Polyhedron>& P);
-void fprint_points(const vector<C_Polyhedron>& P, string fname);
-void fprint_points(const C_Polyhedron& P, string fname, bool append);
 void print_points(const C_Polyhedron& P, ostream& f=cout);
+void print_points(const ninterval_t& P, ostream& f=cout);
+
+
+template <typename T>
+void print_points(const vector<T>& P) {
+    for (const T& p : P) {
+        print_points(p);
+    }
+}
+
+template <typename T>
+void fprint_points(const T& P, string fname, bool append) {
+    ofstream f;
+    if (append) {
+        f.open(fname, ios_base::app);
+    } else {
+        f.open(fname);
+    }
+
+    print_points(P, f);
+}
+
+template <typename T>
+void fprint_points(const vector<T>& P, string fname) {
+    bool append = false;
+    for (const T& p : P) {
+        fprint_points(p, fname, append);
+        append = true;
+    }
+}
+
+#endif
